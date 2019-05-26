cf65d60667d42a212b4c79e731f8b589a589a31a
Gita OFlaherty <gitaoflaherty@gmail.com>
2019-05-26 10:21:10 -0700

    initial commit

---
+++ Tests.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+ #include <vector>
+ #include <string>
+
+ using namespace std;
+
+void Test1()
+{
+    int x = 7;    
+    int *ip = &x;
+    int &y = x;
+    y = 42;
+    int z = 73;
+    ip = &z; // Pointer reassigned to a different memory location
+
+    cout << endl;    
+    cout << "*ip=" << *ip << endl;
+    cout << "x=" << x << endl;
+    cout << "y=" << y << endl;
+    cout << "z=" << z << endl;
+}
+
+
